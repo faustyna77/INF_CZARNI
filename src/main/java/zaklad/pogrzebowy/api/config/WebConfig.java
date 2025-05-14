1552962e89a8ac5081f43f9fbdb9182ac556bee4
Faustyna Misiura <110495453+faustyna77@users.noreply.github.com>
2025-05-14 02:11:35 +0200

    Update WebConfig.java

--- src/main/java/zaklad/pogrzebowy/api/config/WebConfig.java
+++ src/main/java/zaklad/pogrzebowy/api/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://tranquil-dodol-fead8c.netlify.app")"
+                .allowedOrigins("https://tranquil-dodol-fead8c.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
